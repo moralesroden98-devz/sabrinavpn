193dc5a817636387f43f08b8fb26afe3fc1939e2
moralesroden98-devz <moralesroden98@gmail.com>
2026-06-02 16:42:36 +0800

    Update siteOption.js

--- src/contentData/siteOption.js
+++ src/contentData/siteOption.js
@@ -1,11 +1,11 @@
 const siteOptionData = [
     {
       id: 1,
-      siteName: "MEDIATEK APPLICATION",
+      siteName: "SABRINA VPN APPLICATION",
       image:
         "https://i.ibb.co/dsBK8FTw/app-main-logo.png",
       description: "Experience a secure and reliable VPN service with enhanced privacy and performance.",
-      siteLink: "https://sumsag-ultra.site/"
+      siteLink: "https://mediatekvpn.uk/"
     }
   ];
   export default siteOptionData;
